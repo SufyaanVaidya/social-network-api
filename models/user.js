6600a77799e24b99afd13af9362b012e95fc1593
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 19:03:45 -0400

    filled in user schema

--- models/user.js
+++ models/user.js
@@ -3,16 +3,34 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
     {
         username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
             
         },
-        username: {
-            
+        email: {
+        type: String,
+        required: true,
+        unique: true,
         },
-        username: {
-            
+        thoughts: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "thoughts",
+            },
+          ],
+        friends: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "user",
+            },
+          ],
+    },
+    {
+        toJSON: {
+          virtuals: true,
         },
-        username: {
-            
-        },
-    }
+        id: false,
+      }
 )
