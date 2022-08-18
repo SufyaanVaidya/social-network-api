4f54793d0fc954f0dde68b4547a6b6cf6274bd4e
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 15:17:50 -0400

    added rest of thoughts schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,14 +1,20 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
-const thoughtsSchema = new Schema(
+const thoughtsSchema = new mongoose.Schema(
     {
         thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
 
         },
         createdAt: {
-
+            type: Date,
+            default: Date.now,
         },
         username: {
+            type: String,
 
         },
         reactions: {
