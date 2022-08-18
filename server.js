24f6999ae5ccfab7243a9305d8b7e98a169d181a
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 19:55:22 -0400

    completed ther server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const db = require("./config/connection");
+const { user } = require("./models");
+const routes = require("./routes");
+const PORT = 3005;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("API server running on your port ");
+  });
+});
