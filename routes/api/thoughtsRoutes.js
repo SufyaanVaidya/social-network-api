51449469080e7e7e1fd1c3515dec71ab5285af95
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 19:52:06 -0400

    added thoughts routes

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+
+const { createThought, createReaction, getSingleThought , getThoughts , deleteThought , deleteReaction } = require("../../controllers/thoughtsController");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router.route("/:_id").get(getSingleThought).delete(deleteThought);
+
+router.route("/:_id/reactions").post(createReaction);
+
+router.route("/:_id/reactions/:reactionId").delete(deleteReaction);
+
+
+module.exports = router;
