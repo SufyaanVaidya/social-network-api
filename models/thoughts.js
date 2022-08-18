f882c23ac9feaa111f3a63d7ff6b270d81e48d67
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 16:11:56 -0400

    added time to thoughts

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+
+const formatDate = (date) => {
+    return moment(date).format('MMM Do, YYYY -- h:mm a');
+}
 
 const thoughtsSchema = new mongoose.Schema(
     {
@@ -12,6 +17,7 @@ const thoughtsSchema = new mongoose.Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: formatDate,
         },
         username: {
             type: String,
