f5b7cd8146324ae6db39c14594fafda6bb66d627
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-18 16:36:34 -0400

    added reaction schema

--- models/reaction.js
+++ models/reaction.js
@@ -5,3 +5,32 @@ const formatDate = (date) => {
     return moment(date).format('MMM Do, YYYY -- h:mm a');
 };
 
+const reactionSchema = new mongoose.Schema(
+    {
+      reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        max: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: formatDate,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+
+module.exports = reactionSchema;
